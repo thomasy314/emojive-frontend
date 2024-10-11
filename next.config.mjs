8524d8e6aea77ba08e7fc796ede1c600daa45b2e
thomasy314 <thomasy314@gmail.com>
2024-10-10 17:17:44 -0700

    Fresh Start

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-        "BACKEND_ENDPOINT": "127.0.0.1:8080"
-    }
-};
-
-export default nextConfig;
--- utils/emojiUtils/emojiFetchers/const.emojiFetcher.ts
+++
@@ -1,9 +0,0 @@
-
-/**
- * Character used to combine emojis ie 👨 + '\u200d + 👩 + '\u200d + 👧 = 👨‍👩‍👧
- */
-const EMOJI_GLUE_CHARACTER = "\u200d";
-
-export {
-    EMOJI_GLUE_CHARACTER
-};
