0a581a6fba07b3e9565222d3bb9a951c50c291fb
thomasy314 <thomasy314@gmail.com>
2024-11-07 16:09:27 -0800

    Adding router (#3)

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
